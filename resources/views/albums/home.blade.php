7e68c4dd4f0f4d4771202ddfc910e91160592151
chprosdoskimi <henriqueprosdoskimi@hotmail.com>
2021-09-13 15:15:06 -0300

    Music Link navbar

--- resources/views/albums/home.blade.php
+++ resources/views/albums/home.blade.php
@@ -7,28 +7,33 @@
 @section('content')
 
 @if($albuns)
-@forelse ( $albuns as $album)
-<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="{{ url('storage/'.$album->photo)}}" alt="{{$album->name}}" style=" max-width: 100%;">
-    <div class="card-body">
-        <h5 class="card-title">{{$album->name}}</h5>
-        <p class="card-text">Preço R$: {{$album->price}}</p>
-        <p class="card-text">Gênero: {{$album->genre->name}}</p>
-        <p class="card-text">Artista: {{$album->artist->name}}</p>
-        <a class="btn btn-primary" data-bs-toggle="collapse" href="#showMusic{{$album->id}}" role="button" aria-expanded="false" aria-controls="collapseExample">
-            Exibir Músicas
-        </a>
-        <div class="collapse" id="showMusic{{$album->id}}">
-            <div class="card card-body">
-                @if(!$album->music)
-                <ul>
-                    @foreach ( $album->musics as $music )
-                    <li>{{ $loop->index+1 }} - {{$music->name}} {{$music->formated_duration}}</li>
-                    @endforeach
-                </ul>
-                @else
-                <p>Nenhuma música cadastrada</p>
-                @endif
+
+<div class="row">
+    <div style="display: inline-block;">
+        @forelse ( $albuns as $album)
+        <div class="card my-2 " style="width: 18rem">
+            <img class="card-img-top" src="{{ url('storage/'.$album->photo)}}" alt="{{$album->name}}" style=" max-width: 100%;">
+            <div class="card-body">
+                <h5 class="card-title">{{$album->name}}</h5>
+                <p class="card-text">Preço R$: {{$album->price}}</p>
+                <p class="card-text">Gênero: {{$album->genre->name}}</p>
+                <p class="card-text">Artista: {{$album->artist->name}}</p>
+                <a class="btn btn-primary" data-bs-toggle="collapse" href="#showMusic{{$album->id}}" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    Exibir Músicas
+                </a>
+                <div class="collapse" id="showMusic{{$album->id}}">
+                    <div class="card card-body">
+                        @if(!$album->music)
+                        <ul>
+                            @foreach ( $album->musics as $music )
+                            <li>{{ $loop->index+1 }} - {{$music->name}} {{$music->formated_duration}}</li>
+                            @endforeach
+                        </ul>
+                        @else
+                        <p>Nenhuma música cadastrada</p>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -30,6 +30,9 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="/genres/create">Cadastrar Gêneros</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/musics/create">Cadastrar Músicas</a>
+                    </li>
                 </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
